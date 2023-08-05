e599c00d8fcb06314dc65c50c999a9b5148e636e
Yaroslav Zviezda <acroreiser@gmail.com>
2023-08-05 21:31:24 +0300

    fix mismerge

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -1451,11 +1451,6 @@ static struct packet_fanout *fanout_release(struct sock *sk)
 	if (f) {
 		po->fanout = NULL;
 
-		if (atomic_dec_and_test(&f->sk_ref)) {
-			list_del(&f->list);
-			dev_remove_pack(&f->prot_hook);
-			kfree(f);
-		}
 		if (atomic_dec_and_test(&f->sk_ref))
 			list_del(&f->list);
 		else
